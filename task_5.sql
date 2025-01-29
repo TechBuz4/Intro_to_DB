ef81a59f6cf976cadcdf7639d134d6bdc760eff5
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-01-29 14:13:40 -0600

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Populating Your Database with Your Very First Data
+INSERT INTO customer (customer_id, customer_name, email, address) 
+VALUES(1, Cole Baidoo, cbaidoo@sandtech.com, 123 Happiness Ave. )
