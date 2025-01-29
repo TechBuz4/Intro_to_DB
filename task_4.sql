13160e453b702985bc5578128d9cae01b08c9e82
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-01-29 14:08:48 -0600

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,45 +1,4 @@
- -- create the alx database
-CREATE DATABASE IF NOT EXISTS alx_book_store;
--- use the database
-USE alx_book_store;
--- create the tables
-CREATE TABLE Authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
-    author_name VARCHAR(215) NOT NULL
-);
-
-CREATE TABLE Books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DECIMAL(8,2) NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
-);
-
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
-    address TEXT
-);
-
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
-
-CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT NOT NULL,
-    book_id INT NOT NULL,
-    quantity DOUBLE  NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
-);
--- describe the books table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA 
+-- describing the book table
+SELECT COLUMN_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books';
+WHERE TABLE_SCHEMA = 'alx_book_store', "TABLE_NAME = 'Books'
