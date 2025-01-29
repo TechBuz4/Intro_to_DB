85c47c3246455c712b18446e682495f199e99a71
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-01-29 11:41:20 -0600

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -14,7 +14,7 @@ CREATE TABLE Books (
     author_id INT,
     price DECIMAL(8,2) NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE CASCADE
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers (
@@ -26,8 +26,9 @@ CREATE TABLE Customers (
 
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT NOT NULL,
-    order_date DATE NOT NULL,ON DELETE CASCADE
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
@@ -35,6 +36,6 @@ CREATE TABLE Order_Details (
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
